ef5b8eec5f00285c2cee512523cf1acbde4398cc
Shweta45678 <114989483+Shweta45678@users.noreply.github.com>
2023-01-01 01:07:37 +0530

    firstcommit

---
+++ slip1_1.r
@@ -0,0 +1,15 @@
+vector1 = seq(10,40 , length.out=4)
+vector2 = c(20, 10, 40, 40)
+print("Original Vectors:")
+print(vector1)
+print(vector2)
+add= vector1+vector2
+cat("Sum of vector is ",add, "\n")
+sub_vector= vector1-vector2
+cat("Substraction of vector is ",sub_vector, "\n")
+mul_vector= vector1 * vector2
+cat("Multiplication of vector is ",mul_vector, "\n")
+print("Division of two Vectors:")
+[1] "Division of two Vectors:"
+div_vector = vector1 / vector2
+print(div_vector)
